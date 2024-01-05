9de5535714f12ac0c450cdf06dcb075abe8eeb5a
Zachary Dechant <zdechant0621@gmail.com>
2024-01-05 15:40:27 -0700

    Create DB schema, stub book table

---
+++ DB/citylistdb.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS citylistdb;
+CREATE DATABASE IF NOT EXISTS citylistdb;
+
+USE citylistdb;
+
+CREATE TABLE city (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	city VARCHAR(100),
+	state VARCHAR(100),
+	shot_glass TINYINT
+	
+);
+
+DROP USER IF EXISTS traveler@localhost;
+
+CREATE USER IF NOT EXISTS traveler@localhost IDENTIFIED BY 'traveler';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON citylistdb.* TO traveler@localhost;
+
+START TRANSACTION;
+
+INSERT INTO city (id, city, state, shot_glass)
+VALUES (1, 'DENVER', 'COLORADO', 1); -- Assuming TRUE represents a value of 1 for shot_glass
+
+COMMIT;

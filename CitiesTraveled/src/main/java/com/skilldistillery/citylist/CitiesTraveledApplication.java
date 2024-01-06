aab03783244f72a90720b1780ece57cc68bf01a6
Zachary Dechant <zdechant0621@gmail.com>
2024-01-05 17:25:15 -0700

    Config Spring REST project, connect JPA project dependency

---
+++ CitiesTraveled/src/main/java/com/skilldistillery/citylist/CitiesTraveledApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.citylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesTraveledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesTraveledApplication.class, args);
+	}
+
+}
